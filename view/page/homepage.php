2cda3ec1dbaac0c2d5ef664c853c6d1905007cbb
Volkyrie <139378319+Volkyrie@users.noreply.github.com>
2025-06-30 14:18:28 +0200

    add base-html template

--- view/page/homepage.php
+++ view/page/homepage.php
@@ -1,11 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Home page</title>
-</head>
-<body>
-    <h1>Hello World!</h1>
-</body>
-</html>
+<?php
+
+$title = "Page d'accueil";
+// $content = "<h1> Coucou </h1>";
+
+require_once './view/base-html.php';
