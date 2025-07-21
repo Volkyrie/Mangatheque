fcae1896c3b2244dbb115621dfd0846d402ed19c
Volkyrie <139378319+Volkyrie@users.noreply.github.com>
2025-07-21 16:03:41 +0200

    Redirection on homepage if not logged in

--- controller/ControllerPage.php
+++ controller/ControllerPage.php
@@ -1,6 +1,10 @@
 <?php
 class ControllerPage {
     public function homePage() {
+        if(!isset($_SESSION['id'])) {
+            header('Location: /mangatheque/login');
+            exit;
+        }
         $modelUser = new ModelUser();
         $users = $modelUser->getUsers();
         require './view/page/homepage.php';
